c17342aa7d501171fbb48770952bcb4c23776711
yuuka-seki <asyst.seki@gmail.com>
2025-05-29 15:17:21 +0900

    画像を載せるのは難しそう…なので映画のジャンルと評価を載せることにした

--- js/index.js
+++ js/index.js
@@ -3,36 +3,36 @@ const movieTitle = document.getElementById("movie-title");
 const date = document.getElementById("date");
 const comment = document.getElementById("comment");
 const image = document.getElementById("image");
-const saveBtn = document.getElementById("save-btn");
-const resetBtn = document.getElementById("reset-btn");
+const ranking = document.getElementById("ranking");
+const genre = document.getElementById("genre");
 const memoList = document.getElementById("memo-list");
 
+//以下、データを再編集する際によく使うとのことだったのでとりあえず…
+let editingKey = null;
 /*---------------------------------
 
- aveBtnボタンをクリックした時
+ submitボタンをクリックした時
  ①memo-formの内容を取得してlocalStorageに保存する
  ②入力された映画感想をmemo-listに表示させる
- ③画像をBase64形式に変換して表示する
+ ③画像をBase64形式に変換して表示する→諦めた
  可能だったら★で評価するあれを作りたい
 
  ---------------------------------*/
-saveBtn.addEventListener("click", () => {
-	const titleVal = movieTitle.value;
-	const dateVal = date.value;
-	const commentVal = comment.value;
-	const file = image.files[0];
+memoForm.addEventListener("submit", (event) => {
+	event.preventDefault();
 
 	const memoItem = {
-		key1: titleVal,
-		key2: dateVal,
-		key3: commentVal,
-		//どうやら画像をlocalStorageに保存するにはBase64形式に変換する必要があるらしい…
-		//まだ出来ていない
-		key4: file,
+		title: movieTitle.value,
+		date: date.value,
+		comment: comment.value,
+		ranking: ranking.value,
+		genre: genre.value,
 	};
+
 	// 以下、localStorageに保存する
 	// 詰まったこと 一個一個localStorageを取得したら違ったみたいでした
 	// localStorage.setItem(memoItem, JSON.stringify(memoItem));
 	//と、最初に書いたけど、データが上書きされてしまった。
 	localStorage.setItem("review" + Date.now(), JSON.stringify(memoItem));
+	memoForm.reset();
 });
